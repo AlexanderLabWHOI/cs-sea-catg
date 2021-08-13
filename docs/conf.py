315974ebfe9e274909c05fd249380e7fa3cad19c
akrinos <akrinos@vt.edu>
2021-08-13 11:10:49 -0400

    Attempting to update theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
+import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -48,8 +49,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
